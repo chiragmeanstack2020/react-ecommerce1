79fe88de064dcef408df647f93dc15e19b72cee0
chiragmeanstack2020 <chiragmeanstack2020@gmail.com>
2021-02-24 20:10:42 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,45 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <header>
+        <a href="">home</a>
+        <a href="">home</a>
+        <a href="">home</a>
+        <a href="">home</a>
+        <a href="">home</a>
+        <a href="">home</a>
+        <a href="">home</a>
+      </header>
+      <main>
+        <div class="left">
+          <p>
+            men <span>11</span>
+          </p>
+        </div>
+        <div class="right">
+          <div class="filters">
+            <button>
+              <i class="fa fa-sort" /> x
+            </button>
+            <input placeholder="search...." />
+          </div>
+          <div class="products">
+            <div class="item">
+              <img src="" />
+              <div>title</div>
+              <div>old</div>
+              <div>new</div>
+              <div>rating</div>
+              <div>discount</div>
+              <div>tags</div>
+              <div>
+                <i class="fa fa-shopping-cart" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>createed by chirag</footer>
     </div>
   );
 }
